bd42ee5273d76a1c92292ad0ded50af166ae8c02
Bruno Gabriel <bruno.gsantos89@gmail.com>
2014-07-21 23:35:01 -0300

    Added first android project

---
+++ SingleBasicAppTutorial/src/br/com/singletutorial/basic/item1/SingleBasicItem1JSON.java
@@ -0,0 +1,16 @@
+package br.com.singletutorial.basic.item1;
+
+import java.io.Serializable;
+
+/***
+ * This class uses GSON to parser data and put it to see in a List View.
+ * 
+ * @author BrunoGabriel
+ * @since 07/21/2014
+ */
+
+public class SingleBasicItem1JSON implements Serializable {
+
+	private static final long serialVersionUID = 600212725375643082L;
+
+}
